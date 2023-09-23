8a4cb2037b158a20143a0eb70949cec63d02cb4d
juliohq <JulioHenrique31501234@gmail.com>
2023-09-23 11:54:21 -0300

    Adicionar gravidade

---
+++ personagens/personagem.gd
@@ -0,0 +1,14 @@
+extends CharacterBody3D
+
+
+const GRAVIDADE = 16.0
+
+
+func _physics_process(delta):
+	# Aplicar a gravidade ao personagem
+	if is_on_floor():
+		velocity.y = 0.0
+	else:
+		velocity.y -= GRAVIDADE
+	
+	move_and_slide()
