785de14ce32cc68233ab914f1efd13e98c8b3383
juliohq <JulioHenrique31501234@gmail.com>
2023-09-23 17:08:15 -0300

    Adicionar giro e suavidade

--- personagens/personagem.gd
+++ personagens/personagem.gd
@@ -3,6 +3,11 @@ extends CharacterBody3D
 
 const GRAVIDADE = 16.0
 const VELOCIDADE = 16.0
+const VELOCIDADE_GIRO = 2.0 * PI
+
+var olhar = Vector2()
+
+@onready var Modelo = $Modelo
 
 
 func _ready():
@@ -15,6 +20,12 @@ func _physics_process(delta):
 	
 	velocity.x = direcao.x * VELOCIDADE
 	velocity.z = direcao.y * VELOCIDADE
+	
+	# Girar o personagem
+	if direcao:
+		olhar = direcao
+	
+	Modelo.rotation.y = lerp_angle(Modelo.rotation.y, -olhar.angle() + (PI / 2.0), VELOCIDADE_GIRO * delta)
 	
 	# Aplicar a gravidade ao personagem
 	if is_on_floor():
