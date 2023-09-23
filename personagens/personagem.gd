80cbe32ea4cb2614337827a165bbded23cbc3a2e
juliohq <JulioHenrique31501234@gmail.com>
2023-09-23 14:33:11 -0300

    Adicionar movimentação do personagem

--- personagens/personagem.gd
+++ personagens/personagem.gd
@@ -2,6 +2,7 @@ extends CharacterBody3D
 
 
 const GRAVIDADE = 16.0
+const VELOCIDADE = 16.0
 
 
 func _ready():
@@ -9,6 +10,12 @@ func _ready():
 
 
 func _physics_process(delta):
+	# Movimentação do personagem
+	var direcao = Input.get_vector("esquerda", "direita", "frente", "tras")
+	
+	velocity.x = direcao.x * VELOCIDADE
+	velocity.z = direcao.y * VELOCIDADE
+	
 	# Aplicar a gravidade ao personagem
 	if is_on_floor():
 		velocity.y = 0.0
